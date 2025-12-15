7c2f5cfb912e315eb31350b36f1a74688b215b97
Kyrill Willems <kyrill.willems@student.tsaam.be>
2025-12-15 10:14:39 +0100

    server client 1

---
+++ client/js/script.js
@@ -0,0 +1,16 @@
+//html ophalen
+const login = document.querySelector("#login")
+const username = document.querySelector("#username")
+const password = document.querySelector("#password")
+const afterLogin = document.querySelector("#afterLogin")
+const currentPSW = document.querySelector("#currentPSW")
+const newPSW = document.querySelector("#newPSW")
+const loginBtn = document.querySelector("#loginBtn")
+
+
+console.log(loginBtn);
+
+//login als je op de knop klikt
+loginBtn.addEventListener('click', ()=>{
+    
+})
